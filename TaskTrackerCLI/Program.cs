afaa27262b34f2945d40836d2051ccae5141548f
Luan <luanpereiraevangelista@gmail.com>
2026-03-14 11:06:33 -0300

    Implementação inicial da estrutura de comandos da CLI.

--- TaskTrackerCLI/Program.cs
+++ TaskTrackerCLI/Program.cs
@@ -1,2 +1,100 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿if (args.Length == 0)
+{
+    Console.WriteLine("No command provided.");
+    return;
+}
+
+var command = args[0].ToLower();
+
+switch (command)
+{
+    case "add":
+        AddTask(args);
+        break;
+
+    case "update":
+        UpdateTask(args);
+        break;
+
+    case "delete":
+        DeleteTask(args);
+        break;
+
+    case "list":
+        Console.WriteLine("Listing tasks...");
+        break;
+
+    case "mark-done":
+        MarkDone(args);
+        break;
+
+    case "mark-in-progress":
+        MarkInProgress(args);
+        break;
+
+    default:
+        Console.WriteLine($"Command '{command}' not recognized.");
+        break;
+}
+
+void AddTask(string[] args)
+{
+    if (args.Length < 2)
+    {
+        Console.WriteLine("Please provide a task description.");
+        return;
+    }
+
+    var descricao = args[1];
+    Console.WriteLine("Task added successfully");
+}
+
+void UpdateTask(string[] args)
+{
+    if (args.Length < 3)
+    {
+        Console.WriteLine("Please provide a task ID and description.");
+        return;
+    }
+
+    var taskId = args[1];
+    var novaDescricao = args[2];
+
+    Console.WriteLine($"Task {taskId} updated successfully");
+}
+
+void DeleteTask(string[] args)
+{
+    if (args.Length < 2)
+    {
+        Console.WriteLine("Please provide a task ID.");
+        return;
+    }
+
+    var taskId = args[1];
+    Console.WriteLine($"Task {taskId} deleted successfully");
+}
+
+void MarkDone(string[] args)
+{
+    if (args.Length < 2)
+    {
+        Console.WriteLine("Please provide a task ID.");
+        return;
+    }
+
+    var taskId = args[1];
+    Console.WriteLine($"Task {taskId} marked as done.");
+}
+
+void MarkInProgress(string[] args)
+{
+    if (args.Length < 2)
+    {
+        Console.WriteLine("Please provide a task ID.");
+        return;
+    }
+
+    var taskId = args[1];
+    Console.WriteLine($"Task {taskId} marked as in progress.");
+}
